13beba6dc384c8e18ce374a27bbc7b60311d58a3
Abhishek Singh <singhabhishek111213@gmail.com>
2024-04-18 16:26:22 +0530

    Added basic layout with sidebar and main data layout

--- src/Components/MainData/MainData.tsx
+++ src/Components/MainData/MainData.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 function MainData() {
   return (
-    <>MainData</>
+    <div className="basis-9/12 text-black bg-[#f6f6f8] h-full rounded-br-3xl rounded-tr-3xl">MainData</div>
   )
 }
 
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <>Sidebar</>
+    <div className="basis-3/12 flex flex-col items-center text-white h-full rounded-tl-3xl rounded-bl-3xl" style={{background: 'rgba(255,255,255,0.81)'}}>Sidebar</div>
   )
 }
 
