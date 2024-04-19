51774fa4901a4bcb59c197bf22bbe91d1f44aca2
Abhishek Singh <singhabhishek111213@gmail.com>
2024-04-19 16:19:20 +0530

    Completed State builder case

--- src/Redux/Slices/ForecastSlice.ts
+++ src/Redux/Slices/ForecastSlice.ts
@@ -3,7 +3,29 @@ import ForecastDataState from "../../Interfaces/ForecastDataState";
 import axiosInstance from "../../Config/axiosInstance";
 const initialState : ForecastDataState = {
     status: 'default',
-    data: undefined
+    data: {
+        location: {
+            country: '',
+            region: '',
+            name: '',
+            localtime: ''
+        },
+        dayForecast: [],
+        currentData: {
+            uv: 0,
+            wind_kmph: 0,
+            humidity: 0,
+            vis_km: 0,
+            aqi: 0,
+            sunrise: '',
+            sunset: '',
+            temp_c: 0,
+            temp_f: 0,
+            condition: '',
+            is_day: false,
+            chance_of_rain: 0,
+        }
+    },
 }
 
 export const fetchData = createAsyncThunk('data/fetchdata', async()=>{
@@ -23,6 +45,35 @@ const forecastSlice = createSlice({
         builder.addCase(fetchData.fulfilled,(state,action)=>{
             if(!action.payload) return;
             state.status = 'success';
+            const {location,forecast, current} = action.payload.data;
+            // Setting location
+            state.data.location.country = location?.country;
+            state.data.location.region = location?.region;
+            state.data.location.name = location?.name;
+            state.data.location.localtime = location?.localtime;
+
+            state.data.dayForecast = forecast.forecastday.map((forecastItem: any) => {
+                return {
+                    date:forecastItem.date,
+                    avgtemp_c: forecastItem.avgtemp_c,
+                    avgtemp_f:forecastItem.avgtemp_f,
+                    condition:forecastItem.condition.text,
+                }
+            });
+
+            // Setting currentdata
+            state.data.currentData.uv = current.uv;
+            state.data.currentData.wind_kmph = current.wind_kph;
+            state.data.currentData.humidity = current.humidity;
+            state.data.currentData.vis_km = current.vis_km;
+            state.data.currentData.aqi = current.air_quality.pm2_5;
+            state.data.currentData.temp_c = current.temp_c;
+            state.data.currentData.temp_f = current.temp_f;
+            state.data.currentData.is_day = current.is_day;
+            state.data.currentData.condition = current.condition.text;
+            state.data.currentData.sunrise = forecast.forecastday[0].astro.sunrise;
+            state.data.currentData.sunset = forecast.forecastday[0].astro.sunset;
+            state.data.currentData.chance_of_rain = forecast.forecastday[0].day.daily_chance_of_rain;
         })
         .addCase(fetchData.pending,(state)=>{
             state.status = 'loading';
