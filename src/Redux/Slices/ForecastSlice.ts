c986cde82fb6c1cf3c0be046f61279d8e7ba6c23
Abhishek Singh <singhabhishek111213@gmail.com>
2024-04-19 16:00:13 +0530

    Fetch data async thunk

--- src/Redux/Slices/ForecastSlice.ts
+++ src/Redux/Slices/ForecastSlice.ts
@@ -1,17 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import ForecastDataState from "../../Interfaces/ForecastDataState";
-
+import axiosInstance from "../../Config/axiosInstance";
 const initialState : ForecastDataState = {
     status: 'default',
     data: undefined
 }
 
+export const fetchData = createAsyncThunk('data/fetchdata', async()=>{
+    try {
+        const response = await axiosInstance.get(`forecast.json?key=${import.meta.env.VITE_API_KEY}&days=7&aqi=yes`);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 const forecastSlice = createSlice({
     name:'forecast',
     initialState,
     reducers: {},
-    extraReducers: ()=> {}
+    extraReducers: (builder)=> {
+        builder.addCase(fetchData.fulfilled,(state,action)=>{
+            if(!action.payload) return;
+            state.status = 'success';
+        })
+        .addCase(fetchData.pending,(state)=>{
+            state.status = 'loading';
+        })
+    }
 });
 
 export default forecastSlice.reducer;
